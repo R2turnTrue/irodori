bf68c3214a99be0a779bd6b39dc886f6cd72bfde
R2turnTrue <46389044+R2turnTrue@users.noreply.github.com>
2025-09-03 12:53:32 +0900

    feat: gfx base class

--- Irodori/Gfx.cs
+++ Irodori/Gfx.cs
@@ -8,132 +8,159 @@ using Irodori.Texture;
 using Irodori.Type;
 using Irodori.Windowing;
 
-namespace Irodori;
-
-public class Gfx<TBackend, TW> where TBackend: IBackend where TW : Window
+namespace Irodori
 {
-    public class BeforeInitialize
+    public abstract class Gfx
     {
-        internal BeforeInitialize() { }
-        
-        public InitializerWindowingStep WithBackend(TBackend backend)
+        public abstract Window Window
         {
-            return new InitializerWindowingStep(backend);
+            get;
+            protected set;
         }
-    }
-    
-    public class InitializerWindowingStep
-    {
-        private readonly TBackend _backend;
 
-        internal InitializerWindowingStep(TBackend backend)
-        {
-            this._backend = backend;
-        }
-        
-        public InitializerFinalStep WithWindowing(IWindowing<TW> windowing)
-        {
-            return new InitializerFinalStep(_backend, windowing, new Window.InitConfig());
-        }
+        public abstract IBackend Backend { get; }
+
+        public abstract VertexBuffer.Unuploaded CreateVertexBuffer(VertexBufferFormat vertexFormat);
+
+        public abstract ShaderObject.BeforeCompile CreateShader(EShaderType type, string source);
+
+        public abstract ShaderProgram.BeforeLinking CreateShaderProgram();
+
+        public abstract IrodoriState Clear(Color color, FramebufferObject.Uploaded? framebuffer = null);
+
+        public abstract TextureObjectUnuploaded CreateTexture();
+
+        public abstract FramebufferObject.Unuploaded CreateFramebuffer();
     }
 
-    public class InitializerFinalStep
+    public class Gfx<TBackend, TW> : Gfx where TBackend : IBackend where TW : Window
     {
+        public class BeforeInitialize
+        {
+            internal BeforeInitialize() { }
+
+            public InitializerWindowingStep WithBackend(TBackend backend)
+            {
+                return new InitializerWindowingStep(backend);
+            }
+        }
+
+        public class InitializerWindowingStep
+        {
+            private readonly TBackend _backend;
+
+            internal InitializerWindowingStep(TBackend backend)
+            {
+                this._backend = backend;
+            }
+
+            public InitializerFinalStep WithWindowing(IWindowing<TW> windowing)
+            {
+                return new InitializerFinalStep(_backend, windowing, new Window.InitConfig());
+            }
+        }
+
+        public class InitializerFinalStep
+        {
+            private readonly TBackend _backend;
+            private readonly IWindowing<TW> _windowing;
+            private readonly Window.InitConfig _windowConfig;
+
+            internal InitializerFinalStep(TBackend backend, IWindowing<TW> windowing, Window.InitConfig windowConfig)
+            {
+                this._backend = backend;
+                this._windowing = windowing;
+                this._windowConfig = windowConfig;
+            }
+
+            public InitializerFinalStep WithWindowConfig(Window.InitConfig config)
+            {
+                return new InitializerFinalStep(_backend, _windowing, config);
+            }
+
+            public IrodoriReturn<Gfx<TBackend, TW>> Init()
+            {
+                return new Gfx<TBackend, TW>(_backend, _windowing, _windowConfig).Init();
+            }
+        }
+
+        public static BeforeInitialize Create()
+        {
+            return new BeforeInitialize();
+        }
+
         private readonly TBackend _backend;
         private readonly IWindowing<TW> _windowing;
         private readonly Window.InitConfig _windowConfig;
 
-        internal InitializerFinalStep(TBackend backend, IWindowing<TW> windowing, Window.InitConfig windowConfig)
+        public override Window Window { get; protected set; } = null!;
+
+        public override IBackend Backend { get => _backend; }
+
+        private Gfx(TBackend backend, IWindowing<TW> windowing, Window.InitConfig config)
         {
-            this._backend = backend;
-            this._windowing = windowing;
-            this._windowConfig = windowConfig;
-        }
-        
-        public InitializerFinalStep WithWindowConfig(Window.InitConfig config)
-        {
-            return new InitializerFinalStep(_backend, _windowing, config);
+            _backend = backend;
+            _windowing = windowing;
+            _windowConfig = config;
         }
 
-        public IrodoriReturn<Gfx<TBackend, TW>> Init()
+        private IrodoriReturn<Gfx<TBackend, TW>> Init()
         {
-            return new Gfx<TBackend, TW>(_backend, _windowing, _windowConfig).Init();
-        }
-    }
-    
-    public static BeforeInitialize Create()
-    {
-        return new BeforeInitialize();
-    }
-    
-    private readonly TBackend _backend;
-    private readonly IWindowing<TW> _windowing;
-    private readonly Window.InitConfig _windowConfig;
-    
-    public TW Window
-    {
-        get;
-        private set;
-    } = null!; // Cannot be null in normal use. if it is, it's a library bug.
+            var winResult = _windowing.CreateWindow(_windowConfig, _backend);
 
-    private Gfx(TBackend backend, IWindowing<TW> windowing, Window.InitConfig config)
-    { 
-        _backend = backend;
-        _windowing = windowing;
-        _windowConfig = config;
-    }
+            if (winResult.Value == null)
+            {
+                return IrodoriReturn<Gfx<TBackend, TW>>
+                    .Failure(new GeneralNullExceptionError());
+            }
 
-    private IrodoriReturn<Gfx<TBackend, TW>> Init()
-    {
-        var winResult = _windowing.CreateWindow(_windowConfig, _backend);
-        
-        if (winResult.Value == null)
-        {
+            Window = winResult.Value;
+
+            var beResult = _backend.Initialize(Window);
+
+            if (beResult.IsError())
+            {
+                return IrodoriReturn<Gfx<TBackend, TW>>
+                    .NotSure(beResult.Error);
+            }
+
             return IrodoriReturn<Gfx<TBackend, TW>>
-                .Failure(new GeneralNullExceptionError());
+                .Success(this);
         }
 
-        Window = winResult.Value;
-        
-        var beResult = _backend.Initialize(Window);
-
-        if (beResult.IsError())
+        public VertexBuffer.Unuploaded CreateVertexBuffer<TFormat>(TFormat vertexFormat) where TFormat : VertexBufferFormat
         {
-            return IrodoriReturn<Gfx<TBackend, TW>>
-                .NotSure(beResult.Error);
+            return VertexBuffer.Create(_backend, vertexFormat);
         }
 
-        return IrodoriReturn<Gfx<TBackend, TW>>
-            .Success(this);
-    }
+        public override ShaderObject.BeforeCompile CreateShader(EShaderType type, string source)
+        {
+            return ShaderObject.Create(_backend, type, source);
+        }
 
-    public VertexBuffer.Unuploaded CreateVertexBuffer<TFormat>(TFormat vertexFormat) where TFormat : VertexBufferFormat
-    {
-        return VertexBuffer.Create(_backend, vertexFormat);
-    }
-    
-    public ShaderObject.BeforeCompile CreateShader(EShaderType type, string source)
-    {
-        return ShaderObject.Create(_backend, type, source);
-    }
-    
-    public ShaderProgram.BeforeLinking CreateShaderProgram()
-    {
-        return ShaderProgram.Create(_backend);
-    }
-    
-    public IrodoriState Clear(Color color, FramebufferObject.Uploaded? framebuffer = null)
-    {
-        return _backend.Clear(color, Window, framebuffer);
-    }
-    
-    public TextureObjectUnuploaded CreateTexture()
-    {
-        return TextureObjectUnuploaded.Create(_backend);
-    }
-    
-    public FramebufferObject.Unuploaded CreateFramebuffer()
-    {
-        return FramebufferObject.Create(_backend);
+        public override ShaderProgram.BeforeLinking CreateShaderProgram()
+        {
+            return ShaderProgram.Create(_backend);
+        }
+
+        public override IrodoriState Clear(Color color, FramebufferObject.Uploaded? framebuffer = null)
+        {
+            return _backend.Clear(color, Window, framebuffer);
+        }
+
+        public override TextureObjectUnuploaded CreateTexture()
+        {
+            return TextureObjectUnuploaded.Create(_backend);
+        }
+
+        public override FramebufferObject.Unuploaded CreateFramebuffer()
+        {
+            return FramebufferObject.Create(_backend);
+        }
+
+        public override VertexBuffer.Unuploaded CreateVertexBuffer(VertexBufferFormat vertexFormat)
+        {
+            return VertexBuffer.Create(_backend, vertexFormat);
+        }
     }
 }
